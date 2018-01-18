5f8d36f67e85f44e2147fc78859bcf99d241196c
scutums <stepin.ua@gmail.com>
2018-01-18 17:22:15 +0200

    too void

--- app/src/main/java/kh/stepin/alexander/addressbook/DetailFragment.java
+++ app/src/main/java/kh/stepin/alexander/addressbook/DetailFragment.java
@@ -1,8 +1,113 @@
 package kh.stepin.alexander.addressbook;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 /**
  * Created by Alexander on 13.12.2017.
  */
 
-public class DetailFragment {
+public class DetailFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    public interface DetailFragmentListener {
+        void onContactDeleted();
+
+        void onEditContact(Uri contactUri);
+    }
+
+    private static final int CONTACT_LOADER = 0;
+
+    private DetailFragmentListener listener;
+    private Uri contactUri;
+
+    private TextView nameTextView;
+    private TextView phoneTextView;
+    private TextView emailTextView;
+    private TextView streetTextView;
+    private TextView cityTextView;
+    private TextView stateTextView;
+    private TextView zipTextView;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        listener = (DetailFragmentListener) context;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        listener = null;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanseState) {
+        super.onCreateView(inflater, container, savedInstanseState);
+        setHasOptionsMenu(true);
+
+        Bundle arguments = getArguments();
+
+        if (arguments != null) {
+            contactUri = arguments.getParcelable(MainActivity.CONTACT_URI);
+        }
+
+        View view = inflater.inflate(R.layout.fragment_details, container, false);
+
+        nameTextView = (TextView) view.findViewById(R.id.nameTextView);
+        phoneTextView = (TextView) view.findViewById(R.id.phoneTextView);
+        emailTextView = (TextView) view.findViewById(R.id.emailTextView);
+        streetTextView = (TextView) view.findViewById(R.id.streetTextView);
+        cityTextView = (TextView) view.findViewById(R.id.cityTextView);
+        stateTextView = (TextView) view.findViewById(R.id.streetTextView);
+        zipTextView = (TextView) view.findViewById(R.id.zipTextView);
+
+        getLoaderManager().initLoader(CONTACT_LOADER, null, this);
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionMenu(Menu menu, MenuInflater inflater){
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_details_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_edit:
+                listener.onEditContact(contactUri);
+                return true;
+            case R.id.action_delete:
+                deleteContact();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
 }
